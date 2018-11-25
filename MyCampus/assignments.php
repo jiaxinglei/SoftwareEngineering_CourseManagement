972d6af8664c60aafcb1a3decc016fc831165b60
jiaxinglei <36298807+jiaxinglei@users.noreply.github.com>
2018-11-25 14:04:14 +0800

    update

---
+++ MyCampus/assignments.php
@@ -0,0 +1,262 @@
+<?php
+$host="localhost";
+$user="root";
+$pwd="";
+$dbname="course_info";
+
+$conn = new mysqli($host, $user, $pwd, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->query("SET NAMES UTF8");
+
+$sql="SELECT * FROM course_det ";
+$mySQLi_result=$conn->query($sql);
+
+if($mySQLi_result==false){
+    echo ("SQL error");
+    exit;
+}
+
+$rows = [];
+while($row=$mySQLi_result->fetch_array()){
+    $rows[]=$row;
+}
+//var_dump($rows);
+
+$time=time();
+?>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title>MY CAMPUS</title>
+
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+<meta name="author" content="Erwin Aligam - styleshout.com" />
+<meta name="description" content="Site Description Here" />
+<meta name="keywords" content="keywords, here" />
+<meta name="robots" content="index, follow, noarchive" />
+<meta name="googlebot" content="noarchive" />
+
+<link rel="stylesheet" type="text/css" media="screen" href="css/screen.css" />
+    <link href="css/zzsc.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery2.min.js" type=""></script>
+    <script src="js/zzsc.js" type=""></script>
+
+</head>
+<body>
+<a href=" " class="cd-top">Top</a>
+	<!-- header starts-->
+	<div id="header-wrap"><div id="header" class="container_16">			
+				
+		<h1 id="logo-text"><a href="course.php" title="">My Campus</a></h1>
+		<p id="intro">your best course manager...</p>
+		
+		<!-- navigation -->
+		<div  id="nav">
+			<ul>
+				<li><a href="mainpage.php">Home</a></li>
+				<li><a href="course.php">Courses</a></li>
+				<li id="current"><a href="assignments.php">assignments</a></li>
+				<li><a href="http://mail.sjtu.edu.cn">inbox</a></li>
+				<li><a href="account.php">account</a></li>
+                <li><a href="help.html">help</a></li>
+			</ul>		
+		</div>		
+		
+		<div id="header-image"></div> 
+		
+		<form id="quick-search" action="course.php" method="get" >
+			<p>
+			<label for="qsearch">Search:</label>
+			<input class="tbox" id="qsearch" type="text" name="qsearch" value="Search..." title="Start typing and hit ENTER" />
+			<input class="btn" alt="Search" type="image" name="searchsubmit" title="Search" src="images/search.gif" />
+			</p>
+		</form>					
+	
+	<!-- header ends here -->
+	</div></div>
+	
+	<!-- content starts -->
+	<div id="content-outer"><div id="content-wrapper" class="container_16">
+
+		<div id="left-columns" class="grid_4 alpha">
+
+			<div class="grid_4 alpha">
+
+				<div class="sidemenu">
+					<h3>Sidebar Menu</h3>
+					<ul>
+						<li><a href="course.php">Assignments</a></li>
+						<li><a href="course.php#TemplateInfo">Grade</a></li>
+					</ul>
+				</div>
+
+
+			</div>
+
+
+
+			<!-- end left-columns -->
+		</div>
+		<!-- main -->
+		<div id="main" class="grid_8">
+            <style>
+                .comment-data1 {color:red;font-style:italic;font-weight: bold;font-size:15px;}
+                .comment-data2 {color:black;font-style:normal;font-weight:normal;font-size:15px;}
+            </style>
+
+			<h3 id="comments">upcoming assignment</h3>
+
+			<ol class="commentlist">
+
+				<li class="alt" id="comment-63">
+
+                    <cite>
+                        <span class="comment-data1">
+                            <?php echo $rows[0]['courseName']; echo ":<br>";  ?>
+                        </span>
+                        <span class="comment-data2">
+                            <?php  echo "<br>"; echo $rows[0]['Assignments']; ?>
+                        </span>
+                    </cite>
+
+                    <div class="comment-text">
+                        <p><?php  echo date('Y-m-d H:i:s',1542139745)?></p>
+                    </div>
+
+				</li>
+
+				<li id="comment-67">
+
+                    <cite>
+                        <span class="comment-data1">
+                            <?php echo $rows[1]['courseName']; echo ":<br>";  ?>
+                        </span>
+                        <span class="comment-data2">
+                            <?php  echo "<br>"; echo $rows[1]['Assignments']; ?>
+                        </span>
+                    </cite>
+
+                    <div class="comment-text">
+                        <p><?php  echo date('Y-m-d H:i:s',1541939745)?></p>
+                    </div>
+
+				</li>
+
+				<li class="alt" id="comment-71">
+
+                    <cite>
+                        <span class="comment-data1">
+                            <?php echo $rows[4]['courseName']; echo ":<br>";  ?>
+                        </span>
+                        <span class="comment-data2">
+                            <?php  echo "<br>"; echo $rows[4]['Assignments']; ?>
+                        </span>
+                    </cite>
+
+                    <div class="comment-text">
+                        <p><?php  echo date('Y-m-d H:i:s',1541839745  )?></p>
+                    </div>
+
+				</li>
+
+			</ol>
+
+			<h3 id="comments_2">past assignment</h3>
+
+			<ol class="commentlist">
+
+				<li class="alt" id="comment-80">
+
+                    <cite>
+                        <span class="comment-data1">
+                            <?php echo $rows[5]['courseName']; echo ":<br>";  ?>
+                        </span>
+                        <span class="comment-data2">
+                            <?php  echo "<br>"; echo $rows[5]['Assignments']; ?>
+                        </span>
+                    </cite>
+
+                    <div class="comment-text">
+                        <p><?php  echo date('Y-m-d H:i:s',1541735745)?></p>
+                    </div>
+
+				</li>
+
+				<li id="comment-81">
+
+                    <cite>
+                        <span class="comment-data1">
+                            <?php echo $rows[6]['courseName']; echo ":<br>";  ?>
+                        </span>
+                        <span class="comment-data2">
+                            <?php  echo "<br>"; echo $rows[6]['Assignments']; ?>
+                        </span>
+                    </cite>
+
+                    <div class="comment-text">
+                        <p><?php  echo date('Y-m-d H:i:s',1541539789)?></p>
+                    </div>
+				</li>
+
+				<li class="alt" id="comment-82">
+
+                    <cite>
+                        <span class="comment-data1">
+                            <?php echo $rows[7]['courseName']; echo ":<br>";  ?>
+                        </span>
+                        <span class="comment-data2">
+                            <?php  echo "<br>"; echo $rows[7]['Assignments']; ?>
+                        </span>
+                    </cite>
+
+                    <div class="comment-text">
+                        <p><?php  echo date('Y-m-d H:i:s',1541339777)?></p>
+                    </div>
+
+                </li>
+
+			</ol>
+
+
+			<!-- main ends -->
+		</div>
+
+		<!-- contents end here -->
+	</div></div>
+
+		<!-- left-columns starts -->
+
+    <div id="footer-bottom">
+
+        <p class="bottom-left">
+            &nbsp; &copy;2018 All copyright:
+            Design by MYCAMPUS team
+        </p>
+
+        <script>
+            function showemail(){
+                alert('Contact us at: ' +
+                    'mycampus@sjtu.edu.cn')
+            }
+        </script>
+        <p class="bottom-right" >
+            <a href="mainpage.php">Home</a> |
+            <a href="help.html">Help</a> |
+            <a onclick="showemail()">Contact us</a>
+        </p>
+
+    </div>
+			
+	</div>
+	<!-- footer ends here -->
+
+</body>
+</html>
---
+++ MyCampus/mainpage.php
@@ -0,0 +1,272 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jia Xinglei
+ * Date: 17/10/2018
+ * Time: 15:48
+ */
+
+$servername = "localhost";  //此处需填写正确的服务器名称
+$username = "root";  //此处需填写正确的用户名
+$password = "";  //此处需填写正确的密码
+$dbname = "course_info";
+?>
+<?php
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if(! $conn)
+    {
+        die ('connect error:'.mysqli_error($conn));
+    }
+    mysqli_select_db($conn,$dbname)or die("数据库访问错误");
+?>
+
+<!DOCTYPE html >
+
+<html>
+
+<head>
+
+    <title>My Campus</title>
+
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+    <meta name="author" content="Erwin Aligam - styleshout.com" />
+    <meta name="description" content="Site Description Here" />
+    <meta name="keywords" content="keywords, here" />
+    <meta name="robots" content="index, follow, noarchive" />
+    <meta name="googlebot" content="noarchive" />
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/screen.css" />
+
+    <script src="js/jquery2.min.js" type=""></script>
+    <script src="js/zzsc.js" type=""></script>
+
+</head>
+<body>
+
+<!-- header starts-->
+<div id="header-wrap"><div id="header" class="container_16">
+
+        <h1 id="logo-text"><a href="course.php" title="">My Campus</a></h1>
+        <p id="intro">the best course manager...</p>
+
+        <!-- navigation -->
+        <div  id="nav">
+            <ul>
+                <li id ="current"><a href="mainpage.php">Home</a></li>
+                <li><a href="course.php">courses</a></li>
+                <li><a href="assignments.php">assignments</a></li>
+                <li><a href="http://mail.sjtu.edu.cn">inbox</a></li>
+                <li><a href="account_bg.php">account</a></li>
+                <li><a href="help.html">help</a></li>
+            </ul>
+        </div>
+
+        <div id="header-image"></div>
+
+
+
+        <!-- header ends here -->
+    </div></div>
+
+
+
+<div id="content-outer"><div id="content-wrapper" class="container_16">
+
+        <!-- left-columns starts -->
+        <!-- left-columns starts -->
+        <div id="left-columns" class="grid_4 alpha">
+
+            <div class="sidemenu">
+                <h3>Courses</h3>
+                <ul>
+
+                    <?php
+                    session_start();
+                    $major=$_SESSION["major"];
+                    $sql = "select * from major where (major='$major')";
+                    $result=mysqli_query($conn,$sql);
+                    if (!$result) {
+                        printf("Error: %s\n", mysqli_error($conn));
+                        exit();
+                    }
+                    $row = mysqli_fetch_array($result);
+                    $courseId = $row["CId1"];
+                    $sql = "select * from course_det where (courseId='$courseId')";
+                    $result=mysqli_query($conn,$sql);
+                    if (!$result) {
+                        printf("Error: %s\n", mysqli_error($conn));
+                        exit();
+                    }
+                    $rows = mysqli_fetch_array($result);
+                    $courseName=$rows["courseName"];
+                        echo "<li><a href='course/courseInfo.php?id=$courseId'>[" . $courseId . "] " . $courseName . "</a></li>";
+                        $courseId = $row["CId2"];
+                    $sql = "select * from course_det where (courseId='$courseId')";
+                    $result=mysqli_query($conn,$sql);
+                    if (!$result) {
+                        printf("Error: %s\n", mysqli_error($conn));
+                        exit();
+                    }
+                    $rows = mysqli_fetch_array($result);
+                    $courseName=$rows["courseName"];
+                        echo "<li><a href='course/courseInfo.php?id=$courseId'>[" . $courseId . "] " . $courseName . "</a></li>";
+                        $courseId = $row["CId3"];
+                    $sql = "select * from course_det where (courseId='$courseId')";
+                    $result=mysqli_query($conn,$sql);
+                    if (!$result) {
+                        printf("Error: %s\n", mysqli_error($conn));
+                        exit();
+                    }
+                    $rows = mysqli_fetch_array($result);
+                    $courseName=$rows["courseName"];
+                        echo "<li><a href='course/courseInfo.php?id=$courseId'>[" . $courseId . "] " . $courseName . "</a></li>";
+                        $courseId = $row["CId4"];
+                    $sql = "select * from course_det where (courseId='$courseId')";
+                    $result=mysqli_query($conn,$sql);
+                    if (!$result) {
+                        printf("Error: %s\n", mysqli_error($conn));
+                        exit();
+                    }
+                    $rows = mysqli_fetch_array($result);
+                    $courseName=$rows["courseName"];
+                        echo "<li><a href='course/courseInfo.php?id=$courseId'>[" . $courseId . "] " . $courseName . "</a></li>";
+                    ?>
+
+                </ul>
+            </div>
+
+        </div>
+
+
+        <!-- content starts -->
+        <!-- content starts -->
+
+        <!-- main -->
+        <div id="main" class="grid_8">
+
+            <h3><a href="course.php">INFO 1</a></h3>
+
+            <p class="post-info">Posted by <a href="course.php">erwin</a> | Filed under <a href="course.php">templates</a>, <a href="course.php">internet</a>  </p>
+
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+            </p>
+
+            <!--
+            <p class="postmeta">
+            <a href="course.php" class="readmore">Read more</a> |
+            <a href="course.php" class="comments">Comments (3)</a> |
+            <span class="date">July 20, 2008</span>
+            </p>
+            -->
+
+            <h3><a href="course.php">INFO 2</a></h3>
+
+            <p class="post-info">Posted by <a href="course.php">erwin</a> | Filed under <a href="course.php">templates</a>, <a href="course.php">internet</a>  </p>
+
+            <p><a href="http://getfirefox.com/"></a>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+            </p>
+
+            <h3><a href="course.php">INFO 3</a></h3>
+
+            <p class="post-info">Posted by <a href="course.php">erwin</a> | Filed under <a href="course.php">templates</a>, <a href="course.php">internet</a>  </p>
+
+            <p><a href="http://getfirefox.com/"></a>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum.
+            </p>
+
+
+        <!-- main ends -->
+        </div>
+
+
+        <!-- right-columns starts -->
+        <!-- right-columns starts -->
+        <div id="right-columns" class="grid_4 omega">
+
+            <h3>To-Do List</h3>
+
+            <div class="featured-post">
+
+                <h4><a href="course.php">[CS101]Homework 2</a></h4>
+                <p class="post-info">Posted by <a href="course.php">erwin</a><br>due on 09/26/08 </p>
+                <p>
+                    <a href="http://getfirefox.com/"></a>
+                    Vestibulum venenatis. Nulla vel ipsum. Proin rutrum, urna sit
+                </p>
+
+                <p><a class="more-link" href="course.php">more</a></p>
+
+            </div>
+
+            <div class="featured-post">
+
+                <h4><a href="course.php">[IS202]Project 1</a></h4>
+                <p class="post-info">Posted by <a href="course.php">erwin</a><br>due on 09/26/08 </p>
+                <p>
+                    Vestibulum venenatis. Nulla vel ipsum. Proin rutrum, urna sit
+                </p>
+
+                <p><a class="more-link" href="course.php">more</a></p>
+
+            </div>
+
+
+
+            <h3>Coming Up</h3>
+
+            <div class="featured-post">
+                <div class="featured-post">
+
+                    <h4><a href="course.php">Discussion</a></h4>
+                    <p class="post-info">due on 09/26/08 </p>
+                    <p>
+                        Vestibulum venenatis. Nulla vel ipsum. Proin rutrum, urna sit
+                    </p>
+
+                    <p><a class="more-link" href="course.php">more</a></p>
+
+                </div>
+            </div>
+
+
+
+            <!-- end left-columns -->
+        </div>
+
+        <!-- contents end here -->
+    </div></div>
+
+
+<head>
+    <link href="css/zzsc.css" rel="stylesheet" type="text/css" />
+</head>
+
+<a href=" " class="cd-top">Top</a>
+
+<div id="footer-bottom">
+
+    <p class="bottom-left">
+        &nbsp; &copy;2018 All copyright:
+        Design by MYCAMPUS team
+    </p>
+
+    <script>
+        function showemail(){
+            alert('Contact us at: ' +
+                'mycampus@sjtu.edu.cn')
+        }
+    </script>
+    <p class="bottom-right" >
+        <a href="mainpage.php">Home</a> |
+        <a href="help.html">Help</a> |
+        <a onclick="showemail()">Contact us</a>
+    </p>
+
+</div>
+
+</div>
+<!-- footer ends here -->
+
+</body>
+</html>
